d8e2aee9b39cbb7f168682f770e51ca00f6c94e0
rochaandrey <117122109+rochaandrey@users.noreply.github.com>
2025-05-23 01:17:05 -0300

    ta quase

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rochaandrey/restful-API.git/controller"
+	"github.com/rochaandrey/restful-API.git/db"
+	"github.com/rochaandrey/restful-API.git/repository"
 	"github.com/rochaandrey/restful-API.git/usecase"
 )
 
 func main() {
 	server := gin.Default()
 
-	MyProductUseCase := usecase.NewProductUseCase()
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
 
-	ProductController := controller.NewProductController(MyProductUseCase)
+	//inicializacao da camada de repository,usecase e controller
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+	ProductController := controller.NewProductController(ProductUseCase)
 
 	server.GET("/products", ProductController.GetProducts)
 	server.Run(":8080")
